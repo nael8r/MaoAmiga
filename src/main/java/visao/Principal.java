f163ab144c8b91f5ea99cd38a4cc5b54029bf909
nael8r <naelr8@gmail.com>
2016-02-04 12:01:41 -0200

    Modulo para preenchimento do banco com dados aleatórios

--- src/main/java/visao/Principal.java
+++ src/main/java/visao/Principal.java
@@ -1,6 +1,7 @@
 package visao;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -9,32 +10,31 @@ import org.jasypt.util.text.BasicTextEncryptor;
 
 import conexao.HibernateUtil;
 import controle.UsuarioDAO;
+import io.github.benas.jpopulator.api.Populator;
+import io.github.benas.jpopulator.impl.PopulatorBuilder;
+import modelo.Consulta;
+import modelo.Medico;
 import modelo.Usuario;
 
 public class Principal {
-	
-	
-	
+
 	public static void main(String[] args) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-//		
-		UsuarioDAO dao = new UsuarioDAO(session);
+		//Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		Populator pop = new PopulatorBuilder().build();
+		
+		Medico medico = (Medico)pop.populateBean(Medico.class);
+		Consulta consulta = (Consulta)pop.populateBean(Consulta.class);
 //		
 //		Transaction transaction = session.beginTransaction();
-//		
-//		Usuario u = dao.getUsuario(1);
-//		
-//		u.toString();
-//		
 //		transaction.commit();
-		dao.getUsuarios();
-		session.close();
-		
-		
-		
-		System.out.println(util.DateTimeFormat.formatDataParaBR(new Date()));
-		System.out.println(new Date());
-		
+//		
+//		
+//		session.close();
+//
+//		System.out.println(util.DateTimeFormat.formatDataParaBR(new Date()));
+//		System.out.println(new Date());
+
 	}
 
 }
